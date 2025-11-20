97debc6ddc4b38c4b0d273846e6d81ae4679d106
dodo07070707 <aa01056328583@gmail.com>
2025-11-20 15:14:40 +0900

    wrapper hidden fix

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,13 @@ export default function App() {
           </div>
         </div>
       </div>
-
+      <div className="main0-wrapper">
+        내 모든 금융 내역을 한눈에 조회하고 한 곳에서 관리하세요.
+        <br />
+        이제껏 경험 못 했던 쉽고 편리한 금융 서비스,
+        <br />
+        토스와 함께라면 당신의 일상이 새로워질 거예요.
+      </div>
       <main className="main-wrapper">
         <div className="shoe-menu-main">Products</div>
 
