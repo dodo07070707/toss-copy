4570ae989805fb968f7a885ae7aeba7ce91942c8
dodo07070707 <aa01056328583@gmail.com>
2025-11-24 16:28:24 +0900

    product page size error fix

--- src/App.jsx
+++ src/App.jsx
@@ -70,7 +70,7 @@ export default function App() {
           {screenSize === "mobile" ? (
             <ProductMobile />
           ) : screenSize === "tablet" ? (
-            <ProductDesktop />
+            <ProductMobile />
           ) : (
             <ProductDesktop />
           )}
